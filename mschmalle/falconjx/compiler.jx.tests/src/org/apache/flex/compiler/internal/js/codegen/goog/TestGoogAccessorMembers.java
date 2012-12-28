370e4b49032c0a69083f87ad0679997ad9f89b3a
Erik de Bruin <erikdebruin@apache.org>
2012-12-28 16:07:08 +0000

    Adding test for production of 'goog'-ified JS Method Members code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/whiteboard@1426560 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mschmalle/falconjx/compiler.jx.tests/src/org/apache/flex/compiler/internal/js/codegen/goog/TestGoogAccessorMembers.java
@@ -0,0 +1,117 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.flex.compiler.internal.js.codegen.goog;
+
+import org.apache.flex.compiler.clients.IBackend;
+import org.apache.flex.compiler.internal.as.codegen.TestAccessorMembers;
+import org.apache.flex.compiler.internal.js.driver.goog.GoogBackend;
+import org.apache.flex.compiler.tree.as.IAccessorNode;
+import org.junit.Test;
+
+/**
+ * This class tests the production of valid 'goog' JS code for Class Accessor 
+ * members.
+ * 
+ * @author Michael Schmalle
+ * @author Erik de Bruin
+ */
+public class TestGoogAccessorMembers extends TestAccessorMembers
+{
+	// TODO (erikdebruin)
+	//  1) do we have to compile with '--language_in=ECMASCRIPT5'?
+	
+    @Override
+    @Test
+    public void testGetAccessor()
+    {
+    	IAccessorNode node = getAccessor("function get foo():int{return -1;}");
+        visitor.visitFunction(node);
+        //assertOut("Object.defineProperty(A.prototype, 'foo', {get: function() {return -1;}, configurable: true});");
+    }
+
+    @Override
+    @Test
+    public void testGetAccessor_withNamespace()
+    {
+        IAccessorNode node = getAccessor("public function get foo():int{return -1;}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    @Test
+    public void testGetAccessor_withNamespaceOverride()
+    {
+        IAccessorNode node = getAccessor("public override function get foo():int{return -1;}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    @Test
+    public void testGetAccessor_withStatic()
+    {
+        IAccessorNode node = getAccessor("public static function get foo():int{return -1;}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    @Test
+    public void testSetAccessor()
+    {
+    	IAccessorNode node = getAccessor("function set foo(value:int):void{}");
+        visitor.visitFunction(node);
+        //assertOut("Object.defineProperty(A.prototype, 'foo', {set: function(value) {}, configurable: true});");
+    }
+
+    @Override
+    @Test
+    public void testSetAccessor_withNamespace()
+    {
+        IAccessorNode node = getAccessor("public function set foo(value:int):void{}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    @Test
+    public void testSetAccessor_withNamespaceOverride()
+    {
+        IAccessorNode node = getAccessor("public override function set foo(value:int):void{}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    @Test
+    public void testSetAccessor_withStatic()
+    {
+        IAccessorNode node = getAccessor("public static function set foo(value:int):void{}");
+        visitor.visitFunction(node);
+        //assertOut("");
+    }
+
+    @Override
+    protected IBackend createBackend()
+    {
+        return new GoogBackend();
+    }
+}
